fe9795825f94e0bd0ef8e62375bb8212ff0c429f
Michael Michot <michael.michot@pharmasimple.com>
2021-08-26 15:10:29 +0200

    [FIX] Parent_id not found

--- csv_hierarchy_sort/csv_hierachy_sort.py
+++ csv_hierarchy_sort/csv_hierachy_sort.py
@@ -2,19 +2,30 @@ import click
 import csv
 
 
-def compute_complete_path(indexed_csv, current_id, complete_path, parent_id_key='parent_id'):
-    if current_id in indexed_csv and indexed_csv[current_id][parent_id_key]:
-        return compute_complete_path(indexed_csv, indexed_csv[current_id][parent_id_key], f"{indexed_csv[current_id][parent_id_key]}/{complete_path}", parent_id_key)
+def compute_complete_path(
+    indexed_csv, current_id, complete_path, parent_id_key="parent_id"
+):
+    if (
+        current_id in indexed_csv
+        and indexed_csv[current_id][parent_id_key]
+        and indexed_csv[current_id][parent_id_key] in indexed_csv
+    ):
+        return compute_complete_path(
+            indexed_csv,
+            indexed_csv[current_id][parent_id_key],
+            f"{indexed_csv[current_id][parent_id_key]}/{complete_path}",
+            parent_id_key,
+        )
     else:
         return f"{complete_path}"
 
 
 @click.command()
-@click.option('--input_file', help='Input csv file')
-@click.option('--output_file', help='Output csv file')
-@click.option('--id_key', default='id', help='name of id col')
-@click.option('--parent_id_key', default='parent_id', help='name of parent_id col')
-@click.option('--delimiter', default=';', help='CSV delimiter')
+@click.option("--input_file", help="Input csv file")
+@click.option("--output_file", help="Output csv file")
+@click.option("--id_key", default="id", help="name of id col")
+@click.option("--parent_id_key", default="parent_id", help="name of parent_id col")
+@click.option("--delimiter", default=";", help="CSV delimiter")
 def main(id_key, parent_id_key, input_file, output_file, delimiter):
     with open(input_file) as fin:
         reader = csv.DictReader(fin, delimiter=delimiter)
@@ -23,27 +34,31 @@ def main(id_key, parent_id_key, input_file, output_file, delimiter):
 
     for r in indexed_csv:
         if indexed_csv[r][parent_id_key] in indexed_csv:
-            indexed_csv[r]['complete_path'] = compute_complete_path(
-                indexed_csv, r, r, parent_id_key)
+            indexed_csv[r]["complete_path"] = compute_complete_path(
+                indexed_csv, r, r, parent_id_key
+            )
         else:
-            indexed_csv[r]['complete_path'] = r
-            
-    l = [indexed_csv[r]['complete_path'].split(
-        '/') for r in indexed_csv if indexed_csv[r]['complete_path'] != '']
+            indexed_csv[r]["complete_path"] = r
+
+    l = [
+        indexed_csv[r]["complete_path"].split("/")
+        for r in indexed_csv
+        if indexed_csv[r]["complete_path"] != ""
+    ]
     levels = {}
     for s in range(1, max([len(r) for r in l]) + 1):
         levels[s] = list(set([r[s - 1] for r in l if len(r) >= s]))
 
     res = []
     ids = []
-    with open(output_file, 'w') as csvfile:
+    with open(output_file, "w") as csvfile:
 
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=delimiter)
         writer.writeheader()
         for level in levels:
             for id in levels[level]:
                 if id not in ids:
-                    indexed_csv[id].pop('complete_path')
+                    indexed_csv[id].pop("complete_path")
                     writer.writerow(indexed_csv[id])
                     ids.append(id)
 
