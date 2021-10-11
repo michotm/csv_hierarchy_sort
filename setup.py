296ee767e37daafc401d266be9b3ac66d81b151b
Michael Michot <michael.michot@pharmasimple.com>
2021-10-11 15:41:00 +0200

    [FIX] Version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name='CSV Hierarchy Sort',
-    version='0.1.3',
+    version='0.1.4',
     python_requires=">=3.6",
     packages=['csv_hierarchy_sort'],
     install_requires=["click"],
